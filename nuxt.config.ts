e87c4f7fe100944161efac0c0f700102f1c67f1c
CePseudo <hoffmann.axel.6032@gmail.com>
2025-09-15 17:55:18 +0200

    trying to push it on netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
+  ssr: true,
   vite: {
     plugins: [
       tailwindcss(),
