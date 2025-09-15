5b58ee8a51ce9b8fc0778e2dec3b3b9c48ccd5a9
CePseudo <hoffmann.axel.6032@gmail.com>
2025-09-15 18:01:21 +0200

    vercel c'est mieux

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,12 +4,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-  nitro: {
-    prerender: {
-      routes: ['/']
-    }
-  },
-  ssr: true,
   vite: {
     plugins: [
       tailwindcss(),
